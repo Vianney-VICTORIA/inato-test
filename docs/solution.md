27563bcfe4d07353095220a4dc2ab1dfe1574eae
viannneyvictoria <vianneyvictoria@gmail.com>
2023-08-30 09:44:22 +0200

    💻 - Finish CLI

--- docs/solution.md
+++ docs/solution.md
@@ -2,11 +2,11 @@
 
 ## Todo API
 
-- [ ]  Scafold the project with NestJS
+- [x]  Scafold the project with NestJS
 - [ ] Add docs folder and document first choice
-- [ ] Add trials/ endpoint + business Logic
+- [x] Add trials/ endpoint + business Logic
 - [ ] Add Swagger to create postman collection
-- [ ] Add tests
+- [x] Add tests
 
 ## Todo CLI
 
@@ -19,3 +19,12 @@
 - Why Nest JS ? 
 While I was free to decide what to choose for the API it was easy to go for NestJs.
 It's the best NodeJS framework to build API in short period of time. The CLI and other recipe makes the process really fast.
+
+Nest comes with a lot of feature that allow you to divide your code in modules, services, controllers, pipes, guards, interceptors, middlewares, etc.
+It's a really good way to structure your code and make it more readable and maintainable.
+I think for  a large project you could definitely aply to thing : a real DDD approach and the CQRS pattern.
+Here I just used the basic structure of NestJS. DDD and CQRS would be overkill for this project. and would require to spend more time defining the different root aggregates, entities, value objects, etc.
+I would be more than happy to justify this choice in a call 🤗
+
+Testing Strategy:
+- Jest is the default testing framework for NestJS. Here I am test My controller and so my DTO, Then I am testing my service and so my business logic. 
--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,12 +1,82 @@
-import { program } from "commander";
+#! /usr/bin/env node
+
+import { program } from 'commander'
+import { textSync } from 'figlet'
+
+interface ITrial {
+  name: string
+  country: string
+  start_date: Date
+  end_date: Date
+  sponsor: string
+  canceled: boolean
+  study_type: string
+  primary_purpose: string
+}
+
+const getTrials = async (
+  path: string,
+  options?:
+    | {
+        sponsor?: string
+        countryCode?: string
+        table?: boolean
+      }
+    | undefined
+) => {
+  try {
+    let apiPath = path
+
+    if (options?.sponsor) {
+      apiPath = `${apiPath}?sponsor=${options.sponsor}`
+    }
+
+    if (options?.countryCode) {
+      apiPath = `${apiPath}?countryCode=${options.countryCode}`
+    }
+
+    const response = await fetch(`http://localhost:3000/${apiPath}`)
+
+    const dataTrials = await response.json().then((data) => {
+      return data.data
+    })
+
+    if (options?.table) {
+      const formatedResponse = dataTrials.map(async (trial: ITrial) => {
+        const { name, country, start_date, end_date, sponsor, canceled, study_type, primary_purpose } = trial
+        return {
+          name,
+          country,
+          start_date,
+          end_date,
+          sponsor,
+          canceled,
+          study_type,
+          primary_purpose
+        }
+      })
+      console.table(await Promise.all(formatedResponse))
+    } else {
+      console.log(dataTrials)
+    }
+  } catch (error) {
+    console.error('Error occurred while fetching the data!', error)
+  }
+}
+
+console.log(textSync('Inato CLI', { horizontalLayout: 'full' }))
+
+program.name('inato-cli').description('CLI to Inato').version('1.0.0')
 
 program
-  .name("inato-cli")
-  .command("trials")
-  .description("get the list of clinical trials")
-  .requiredOption("-p, --path <type>", "the api path to call")
-  .action(async function ({ path }) {
-    const response = await fetch(`http://localhost:8080/${path}`);
-    console.log(await response.text());
+  .command('trials')
+  .description('Get a list of clinical trials')
+  .argument('<path>', 'the path to the api')
+  .option('-s, --sponsor <string>', 'Filter by sponsor name')
+  .option('-c, --countryCode <string>', 'Filter by country code')
+  .option('-t, --table <boolean>', 'Display the result in a table')
+  // add action that call async function with 2 parameters: path and options where options can be optional
+  .action(async (path, options?: { sponsor?: string; countryCode?: string; table?: boolean }) => {
+    await getTrials(path, options)
   })
-  .parseAsync(process.argv);
+  .parseAsync(process.argv)
